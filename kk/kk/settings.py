9c6e38b6557015363d8e7f42d404c06e45e3d1ac
krishna karki <krishna karki>
2017-02-28 16:00:38 -0600

    SOme changes

--- kk/kk/settings.py
+++ kk/kk/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME' : 'blog',
         'USER' : 'root',
-        'PASSWORD' : 'root',
+        'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT' : '3306'
     }
